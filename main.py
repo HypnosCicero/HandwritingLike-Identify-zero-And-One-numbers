2f24c47bac471a1dde5298c6a95ca5bf823a9832
cinSophomore <1345764659@qq.com>
2022-07-27 16:05:59 +0800

    feat : Create the modle of collecting data sets (have some bugs)

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import torch.nn as nn
+import xlrd
 
-# First of all,we still need to consider the input sample data,
-# and use it as the base for building the neural network
+# Collecting data sets (搜集数据集)
+def readAllData(filename):
+    readxls = xlrd.open_workbook(filename)
+    sheet1 = readxls.sheet_by_name('Data')
+    nrows = sheet1.nrows
+    for i in range(nrows):
+        print(i)
+
+
+filename = "E:\Work\Test_System\Identify_0_And_1_numbers\DataA.xlsx"
+readAllData(filename)
 
 # define the input number
 N=0 # numbers of groups witch is the test data
