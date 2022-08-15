b8e498df7f88f91c36137d9982854e10b7a14970
cinSophomore <1345764659@qq.com>
2022-08-15 08:41:38 +0800

    fix : Fixed the bug about getting data

--- main.py
+++ main.py
@@ -3,36 +3,63 @@ import torch.nn as nn
 import xlrd
 
 # Collecting data sets (搜集数据集)
+
+X = torch.tensor([64, 12])
+Y = torch.tensor([12])
 def readAllData(filename):
-    readxls = xlrd.open_workbook(filename)
-    sheet1 = readxls.sheet_by_name('Data')
-    cell = sheet1.cell_value(4, 10)
-    print("cell = ")
-    print(cell)
-    nrows = sheet1.nrows
-    '''
-    for i in range(nrows):
-        rowList=sheet1.row_values(i) # 按行遍历出来所有数据。
-        print("这之前")
-        print(rowList)
-        for j in range(rowList.__sizeof__()):
-            #k = 0
-            print("list内部")
-            print(rowList[j])
-        #print('在这之后:' + rowList)
-    '''
+    readXls = xlrd.open_workbook(filename)
+    sheet1 = readXls.sheet_by_name('Data')
+    data_row = 4
+    data_column = 10
+    value_row = 8
+    value_column = 13
+    # cell = sheet1.cell_value(4, 10)
+    XList = []
+    YList = []
+    for i in range(64):
+        xlist = []
+        if i > 0 :
+            data_row -= 4
+            data_column += 4
+        myFlag = 0
+        for j in range(12):
+            xvalue = sheet1.cell_value(data_row, data_column)
+            print("x value is " + str(xvalue))
+            xlist.append(xvalue)
+            myFlag += 1
+            if myFlag == 3:
+                data_column -= 2
+                data_row += 1
+                myFlag = 0
+            else:
+                data_column += 1
+        XList.append(xlist)
+        yvalue = sheet1.cell_value(value_row, value_column)
+        YList.append(yvalue)
+        print("y value is " + str(yvalue))
+        value_column += 4
+        print("\n")
+
+    X = torch.tensor(XList, dtype=torch.long)
+    Y = torch.tensor(YList, dtype=torch.long)
+    print(X)
+    print(Y)
 
 
 filename = "E:\Work\Test_System\Identify_0_And_1_numbers\DataA.xlsx"
 readAllData(filename)
+
 
 # define x and y
 x = 100
 y = 100
 
 
+
 # define the input number
 N = 10  # numbers of groups witch is the test data
+
+# in this model the N is  3*4(meening is 3c 4r)
 
 model = nn.Sequential(
     nn.Linear(N, 10, True),
